b4b80b57558ba41e9e06ee0c7ab6e7d8756365e2
jarzynka <jarzynka@outlook.com>
2014-01-16 23:08:41 -0500

    Made Window Frame larger and made line longer to match

--- SwingGraphics/src/com/outlook/jim/JustaFrame.java
+++ SwingGraphics/src/com/outlook/jim/JustaFrame.java
@@ -10,12 +10,12 @@ public class JustaFrame extends Frame {
 	}
 	
 	public void Paint(Graphics g){
-		g.drawLine(10, 10, 150, 150);
+		g.drawLine(10, 10, 300, 300);
 	}
 	
 	public static void main(String[] arg){
 		JustaFrame frame = new JustaFrame();
-		frame.setSize(200, 200);
+		frame.setSize(300, 300);
 		frame.setVisible(true);
 		//this is a test
 	}
